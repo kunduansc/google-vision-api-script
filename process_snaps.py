9ca0dc857a3c8a0d9aca38fd052cbf177aa17f32
Sushobhan Nayak <sushobhan.nayak@snapchat.com>
2016-04-27 17:43:06 +0000

    some changes: removed face detection. added html processing only option. put adult subject screening

--- process_snaps.py
+++ process_snaps.py
@@ -26,7 +26,7 @@ django.setup()
 # Task feature dictionaries
 MAX_RESULTS = 10
 features = {}
-features['face_detection'] = {'type': 'FACE_DETECTION', 'maxResults': MAX_RESULTS}
+#features['face_detection'] = {'type': 'FACE_DETECTION', 'maxResults': MAX_RESULTS}
 features['landmark_detection'] = {'type': 'LANDMARK_DETECTION', 'maxResults': MAX_RESULTS}
 features['logo_detection'] = {'type': 'LOGO_DETECTION', 'maxResults': MAX_RESULTS}
 features['label_detection'] = {'type': 'LABEL_DETECTION', 'maxResults': MAX_RESULTS}
@@ -84,8 +84,8 @@ def highlight_image(image, response, output_folder, tasks):
     im.save(output_folder + '/original_image.jpg')
     draw = ImageDraw.Draw(im)
 
-    if 'all' in tasks or 'face_detection' in tasks:
-        draw_rectangle(draw, response, 'faceAnnotations', '#00ff00', 'fdBoundingPoly')
+    #if 'all' in tasks or 'face_detection' in tasks:
+     #   draw_rectangle(draw, response, 'faceAnnotations', '#00ff00', 'fdBoundingPoly')
     if 'all' in tasks or 'landmark_detection' in tasks:
         draw_rectangle(draw, response, 'landmarkAnnotations', '#7cedff')
     if 'all' in tasks or 'logo_detection' in tasks:
@@ -97,14 +97,23 @@ def highlight_image(image, response, output_folder, tasks):
     im.save(output_folder + '/edited_image.jpg')
 
 
+def update_data(data, key, key_string, response):
+    try:
+        data[key] = [x['description'] for x in response['responses'][0].get(key_string, [])]
+    except:
+        pass
+
 def log_metadata(response, output_folder):
     json.dump(response, open(output_folder + '/all.json', 'w'))
     data = {}
-    data['labels'] = [x['description'] for x in response['responses'][0].get('labelAnnotations', [])]
-    data['faces'] = len(response['responses'][0].get('faceAnnotations', []))
-    data['landmarks'] = [x['description'] for x in response['responses'][0].get('landmarkAnnotations', [])]
-    data['logos'] = [x['description'] for x in response['responses'][0].get('logoAnnotations', [])]
-    data['primary_text'] = [x['description'] for x in response['responses'][0].get('textAnnotations', [{'description':''}])][0]
+    update_data(data, 'labels', 'labelAnnotations', response)
+    #data['faces'] = len(response['responses'][0].get('faceAnnotations', []))
+    update_data(data, 'landmarks', 'landmarkAnnotations', response)
+    update_data(data, 'logos', 'logoAnnotations', response)
+    try:
+        data['primary_text'] = [x['description'] for x in response['responses'][0].get('textAnnotations', [{'description':''}])][0]
+    except:
+        pass
     json.dump(data, open(output_folder + '/selective.json', 'w'))
 
 def process_image(image_file, service, output_file_path, tasks=['all']):
@@ -170,18 +179,42 @@ def process_html(folder_path):
 
     video_folders = glob.glob(folder_path + '/_processed_video_*')
     image_folders = glob.glob(folder_path + '/_processed_image_*')
+    print "Creating html with processed video and image folders: "
+    print video_folders
+    print image_folders
 
     for video_folder in video_folders:
+        is_adult = False
         video_folder = os.path.abspath(video_folder)
         file_name = video_folder.split('_processed_video_')[-1]
-        query_media = [video_folder + '/' + file_name + '.mp4', 'mp4']
+        tags = "" if not os.path.isfile(video_folder + '/tags.data') else open(video_folder + '/tags.data').readline()
+        query_media = [video_folder + '/' + file_name + '.mp4', 'mp4', tags]
         match_media_list = []
         images = glob.glob(video_folder + '/_processed_image_*')
         for image in images:
-            match_media_list.append((image+'/edited_image.jpg', 'jpg', str(json.load(open(image + '/selective.json')))))
+            if image.split('-')[-1] not in ['1', '4', '7']:
+                continue
+            data = json.load(open(image + '/selective.json'))
+            data_dict = {}
+            if 'primary_text' in data and len(data['primary_text']) > 0:
+                data_dict['text: '] = data['primary_text'].encode('ascii', 'ignore')
+            if 'logos' in data and len(data['logos']) > 0:
+                data_dict['logo: '] = str(data['logos']) 
+            if 'landmarks' in data and len(data['landmarks']) > 0:
+                data_dict['landmarks: '] = str(data['landmarks']) 
+            if 'labels' in data and len(data['labels']) > 0:
+                data_dict['labels: '] = str(data['labels']) 
+            all_data = json.load(open(image + '/all.json'))
+            if 'safeSearchAnnotation' in all_data['responses'][0]:
+                data_dict['safeSearchAnnotation'] = str(all_data['responses'][0]['safeSearchAnnotation'])
+                if all_data['responses'][0]['safeSearchAnnotation']['adult'] == 'POSSIBLE':
+                    is_adult = True
+
+            match_media_list.append((image+'/edited_image.jpg', 'jpg', data_dict))
         block_context = Context( {"query_media": query_media,
                                   "match_media_list": match_media_list})
-        block_html.append(block_template.render(block_context))
+        if not is_adult:
+            block_html.append(block_template.render(block_context))
 
     for image_folder in image_folders:
         image_folder = os.path.abspath(image_folder)
@@ -198,15 +231,23 @@ def process_html(folder_path):
         
 
 def main(input_file_path, output_file_path):
+    processed_videos = [x.split('/')[-1].split('_processed_video_')[-1] for x in glob.glob(output_file_path+"/_processed_video_*")]
+    processed_images = [x.split('/')[-1].split('_processed_image_')[-1] for x in glob.glob(output_file_path+"/_processed_image_*")]
     service = get_vision_service()
     if os.path.isfile(input_file_path):
         dispatch_file(input_file_path, service, output_file_path)
+        pass
     elif os.path.isdir(input_file_path):
         print 'processing folder: ', input_file_path
         for path in os.listdir(input_file_path):
+            if path.split('.')[0] in processed_videos or path.split('.')[0] in processed_images:
+                print "EXISTS: ", path
+                continue
             input_file = input_file_path + '/' + path
             if os.path.isfile(input_file):
                 dispatch_file(input_file, service, output_file_path)
+                pass
+
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -216,8 +257,11 @@ if __name__ == '__main__':
     parser.add_argument(
         '--out', dest='output', default=os.getcwd(),
         help='the name of the output file.')
-
+    parser.add_argument(
+        '--html_only', dest='html_only', default=False,
+        help='use this if you only want to process the outputs for html')
     args = parser.parse_args()
-
-    main(args.input_path, args.output)
+    
+    if not (args.html_only):
+        main(args.input_path, args.output)
     process_html(args.output)
--- test/example.sh
+++ test/example.sh
@@ -1,4 +1,4 @@
 rm -rf _processed_video_*
 rm -rf _processed_image_*
-python ../process_snaps.py .
+time python ../process_snaps.py .
 echo "Result is in result.html"
